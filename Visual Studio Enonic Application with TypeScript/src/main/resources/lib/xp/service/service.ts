58cc1a4b437068faa89f0b34f87d80f3c7bcfef9
krelde <krelde87@gmail.com>
2017-04-20 15:00:02 +0200

    Added service base class

---
+++ Visual Studio Enonic Application with TypeScript/src/main/resources/lib/xp/service/service.ts
@@ -0,0 +1,59 @@
+﻿
+
+'use strict';
+
+import portal = require("xp/portal");
+
+
+
+export abstract class Service {
+
+    
+    
+    portalLib: lib.xp.portal;
+    contentType: string;
+    body: any;
+    method: any;
+
+    params:any;
+    constructor(request: any) {
+        
+        this.portalLib = portal;
+        this.contentType = 'application/json';
+        this.method = request.method;
+        this.params = this.parseparams(request.params);
+    }
+
+    get() {
+        return this; // chainable
+    }
+
+    post() {
+        return this; // chainable
+    }
+
+    buildResponse() {
+        switch (this.method) {
+            case 'GET': this.get(); break;
+            case 'POST': this.post(); break;
+        }
+        
+        this.body = this.createresults(this.getItems(), this.params);
+        
+        this.response = {
+            body: this.body,
+            contentType: this.contentType
+        };
+
+        return this;
+    }
+
+    abstract createresults(items, params, total?);
+
+    abstract getItems();
+
+    abstract parseparams(params);
+
+    public static handleRequest(request: any): any { }
+} 
+
